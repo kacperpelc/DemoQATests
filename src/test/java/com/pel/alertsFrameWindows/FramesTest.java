10829693b28738e594ed1f7eeb971dddc7064d7c
kacperpelc <worngnoe@gmail.com>
2022-09-21 14:15:14 +0200

    - ADDED AlertsFramesWindows tags

--- src/test/java/com/pel/alertsFrameWindows/FramesTest.java
+++ src/test/java/com/pel/alertsFrameWindows/FramesTest.java
@@ -2,9 +2,10 @@ package com.pel.alertsFrameWindows;
 
 import com.pel.foundation.Base;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
-
+@Tags({@Tag("regression"), @Tag("alerts_frames_windows"), @Tag("frames")})
 public class FramesTest extends Base {
     @Tag("131")
     @Test
