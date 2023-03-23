f1035f66b3a6c7d7637334b94f402e6889945141
Kacper Pelc <kacperpelc@mail.com>
2023-03-23 19:40:59 +0100

    - UPDATED pom file - FIXED issue "Unable to establish websocket connection" - REMOVED deprecated headless mode

--- src/main/java/com/pel/foundation/browserOptions/ChromeOptionsOwn.java
+++ src/main/java/com/pel/foundation/browserOptions/ChromeOptionsOwn.java
@@ -7,8 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ChromeOptionsOwn {
-    private static final boolean headless = System.getProperty("browser.headless").equalsIgnoreCase("true");
-
     public static ChromeOptions get() {
         Map<String, Object> preferences = new HashMap<>();
         preferences.put("profile.default_content_settings.popups", 0);
@@ -16,8 +14,8 @@ public class ChromeOptionsOwn {
         preferences.put("download.directory_upgrade", true);
 
         ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--remote-allow-origins=*");
         options.setExperimentalOption("prefs", preferences);
-        options.setHeadless(headless);
         return options;
     }
 }
--- src/main/java/com/pel/foundation/browserOptions/EdgeOptionsOwn.java
+++ src/main/java/com/pel/foundation/browserOptions/EdgeOptionsOwn.java
@@ -1,15 +1,12 @@
 package com.pel.foundation.browserOptions;
 
 import com.pel.utilities.Constants;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class EdgeOptionsOwn {
-    private static final boolean headless = System.getProperty("browser.headless").equalsIgnoreCase("true");
-
     public static EdgeOptions get() {
         Map<String, Object> preferences = new HashMap<>();
         preferences.put("profile.default_content_settings.popups", 0);
@@ -18,7 +15,6 @@ public class EdgeOptionsOwn {
 
         EdgeOptions options = new EdgeOptions();
         options.setExperimentalOption("prefs", preferences);
-        options.setHeadless(headless);
         return options;
     }
 }
