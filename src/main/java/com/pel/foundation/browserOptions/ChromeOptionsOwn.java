836b6a77aa0c78407873785eb5145ba24626897e
Kacper Pelc <kacperpelc@mail.com>
2023-04-15 13:43:26 +0200

    - UPDATED CICD config

--- src/main/java/com/pel/foundation/browserOptions/ChromeOptionsOwn.java
+++ src/main/java/com/pel/foundation/browserOptions/ChromeOptionsOwn.java
@@ -15,6 +15,7 @@ public class ChromeOptionsOwn {
 
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("prefs", preferences);
+        options.addArguments("--disable-dev-shm-usage");
         return options;
     }
 }
